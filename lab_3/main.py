2dc2bd65c8b0a18f7c56c21b808fc7ffc54f522d
ddanilyuk <danis.danilyuk@gmail.com>
2019-10-05 22:37:24 +0300

    [add] lab3

--- lab_3/main.py
+++ lab_3/main.py
@@ -1,6 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 
-"""
-******************************
-**** Here must be 3 lab ******
-******************************
-"""
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.set_aspect("equal")
+
+
+def f(x, y):
+    return np.sin(x) * np.cos(y)
+
+
+x = np.linspace(-6, 6, 30)
+y = np.linspace(-6, 6, 30)
+
+X, Y = np.meshgrid(x, y)
+
+Z = f(X, Y)
+print(Z)
+
+ax.contour3D(X, Y, Z, 50, cmap='binary')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+
+plt.show()
