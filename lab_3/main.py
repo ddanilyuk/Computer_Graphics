0c9b7f5346abb0fe27335011d3a47320e187f1a0
ddanilyuk <danis.danilyuk@gmail.com>
2019-10-09 01:16:47 +0300

    [add] lab3

--- lab_3/main.py
+++ lab_3/main.py
@@ -1,27 +1,24 @@
-import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
-
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-ax.set_aspect("equal")
 
 
 def f(x, y):
     return np.sin(x) * np.cos(y)
 
 
-x = np.linspace(-6, 6, 30)
-y = np.linspace(-6, 6, 30)
+x = np.linspace(-6, 6, 60)
+y = np.linspace(-6, 6, 60)
 
 X, Y = np.meshgrid(x, y)
 
 Z = f(X, Y)
-print(Z)
+fig = make_subplots(rows=1, cols=1,
+                    specs=[[{'is_3d': True}]],
+                    subplot_titles=['Lab3'],
+                    )
+fig.layout.autosize = True
 
-ax.contour3D(X, Y, Z, 50, cmap='binary')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
-
-plt.show()
+fig.add_trace(go.Surface(x=X, y=Y, z=Z, opacity=0.7, surfacecolor=Z))
+fig.update_layout(title_text="Lab3")
+fig.show()
