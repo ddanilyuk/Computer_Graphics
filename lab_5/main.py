409880a2328ec4abe229f8563b09c41316309b94
ddanilyuk <danis.danilyuk@gmail.com>
2019-11-13 22:47:29 +0200

    [edit] lab5

--- lab_5/main.py
+++ lab_5/main.py
@@ -1,92 +1,96 @@
-import tkinter as tk
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import numpy as np
-import argparse
+from math import fabs
 import random
 
-from lab_5.movingModel import MovingModel as MV_model
+
+plt.style.use('dark_background')
+
+# fig = plt.figure
+fig, ax = plt.subplots()
+ax = plt.axes(xlim=(-60, 60), ylim=(-60, 60))
+line1, = ax.plot([], [], lw=2, color='blue')
+line2, = ax.plot([], [], lw=2, color='violet')
+ball1, = ax.plot([], [], color='blue', marker='o', linestyle='dashed', linewidth=2, markersize=33)
+ball2, = ax.plot([], [], color='violet', marker='o', linestyle='dashed', linewidth=2, markersize=33)
 
 
-def main(A, B, R, size) -> None:
-    """
-    Load window with 2 moving figures
-
-    :param A: size of A
-    :param B: size of B
-    :param R: size of R
-    :param size: scale size
-    """
-
-    # Creating root and canvas
-    window = tk.Tk()
-    canvas = tk.Canvas(window, width=1200, height=900, bg='lightgrey')
-    canvas.pack()
-
-    # Formula for figure coords
-    formula_for_coords = {
-        'x': lambda t: (A * R * (np.sin(t)) ** 3) * size,
-        'y': lambda t: (B * R * (np.cos(t)) ** 3) * size,
-    }
-
-    # Some figures's params
-    speed_1 = {'x': 10, 'y': 5}
-    speed_2 = {'x': 5, 'y': 10}
-
-    # Color for random choosing
-    colors = ['black', 'yellow', 'red', 'brown', 'Navy Blue', 'violet', 'orange']
-
-    # Create 2 models with different start points
-    model1 = MV_model(
-        canvas, formula_for_coords, speed_1,
-        center=(random.randint(100, 400), random.randint(100, 300)),
-        timeon=25)
-    model2 = MV_model(
-        canvas, formula_for_coords, speed_2,
-        center=(random.randint(600, 1000), random.randint(500, 800)),
-        timeon=25)
-
-    def check_distance():
-        """
-        Check if figures are close enough
-        and change their speed to opposite by direction
-        """
-        center1 = model1.get_center()
-        center2 = model2.get_center()
-
-        if (
-                abs(center1[0] - center2[0]) <= 100 and
-                abs(center1[1] - center2[1]) <= 100
-        ):
-            model1.change_direction(x=True, y=True)
-            model1.change_color(
-                colors[random.randint(0, len(colors)) - 1])
-
-            model2.change_direction(x=True, y=True)
-            model2.change_color(
-                colors[random.randint(0, len(colors) - 1)])
-
-        canvas.after(50, check_distance)
-
-    # Set timer for checking
-    canvas.after(50, check_distance)
-
-    # Start program
-    window.mainloop()
+# Функция инициализации.
+def init():
+    # создение пустого графа.
+    line1.set_data([], [])
+    line2.set_data([], [])
+    return line1, line2,
 
 
-# Some cli params handles
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser('Built 2 moving figures')
+xdata, ydata = [], []
+xdata2, ydata2 = [], []
 
-    parser.add_argument(
-        '--A', type=float, default=10)
-    parser.add_argument(
-        '--B', type=float, default=20)
-    parser.add_argument(
-        '--R', type=float, default=5)
-    parser.add_argument(
-        '--size', help="Scale your chart", type=float, default=1)
+# Color for random choosing
+colors = ['white', 'yellow', 'red', 'brown', 'blue', 'violet', 'orange']
 
-    args = parser.parse_args()
 
-    # Starting point of main script with given params
-    main(args.A, args.B, args.R, args.size)
+# функция анимации
+def animate(i):
+    tt = 0.1 * i
+    tt2 = 0.05 * i
+
+    print(i)
+
+    # x, y данные на графике
+
+    # First trajectory
+    x1 = 40 * np.sin(tt) ** 3
+    y1 = 40 * np.cos(tt) ** 3
+
+    bx1 = 40 * np.sin(tt) ** 3
+    by1 = 40 * np.cos(tt) ** 3
+
+    # Second trajectory
+    x2 = 40 * np.sin(tt2) ** 3 + 10
+    y2 = 40 * np.cos(tt2) ** 3 + 6
+
+    bx2 = 40 * np.sin(tt2) ** 3 + 10
+    by2 = 40 * np.cos(tt2) ** 3 + 6
+
+    # добавление новых точек в список точек осей x, y
+    xdata.append(x1)
+    ydata.append(y1)
+    line1.set_data(xdata, ydata)
+
+    ball1.set_data(bx1, by1)
+
+    xdata2.append(x2)
+    ydata2.append(y2)
+    line2.set_data(xdata2, ydata2)
+    ball2.set_data(bx2, by2)
+
+    difference_x = fabs(x1 - x2) * 10
+    difference_y = fabs(y1 - y2) * 10
+
+    # print(difference_x, "\n", difference_y)
+
+    if (difference_x < 120 and difference_y < 120):
+        new_color1 = colors[random.randint(0, len(colors)-1)]
+        new_color2 = colors[random.randint(0, len(colors)-1)]
+        ball1.set(color=new_color1)
+        ball2.set(color=new_color2)
+
+
+        print("YES")
+
+    return line1, line2, ball1, ball2
+
+
+# Заголовок анимации
+plt.title('Lab_5')
+# Скрываем лишние данные
+plt.axis('off')
+
+
+# Вызов анимации.
+anim = animation.FuncAnimation(fig, animate, init_func=init,
+                               frames=1000, interval=70, blit=True)
+
+plt.show()
